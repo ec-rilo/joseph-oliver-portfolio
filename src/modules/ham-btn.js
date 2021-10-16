0ee047aa2bf2ee7756554e5c0936d7f57d25eb2a
Edgar <ecarrillo046@gmail.com>
2021-10-16 14:35:10 -0700

    Add a toggleHamBtn function

--- src/modules/ham-btn.js
+++ src/modules/ham-btn.js
@@ -2,10 +2,19 @@ import { pageLogic } from './page-switch-logic.js';
 
 const hamBtnLogic = (() => {
   let hamBtn = document.querySelector('.nav-icon4');
+
+  function toggleHamBtn() {
+    hamBtn.classList.toggle('open');
+  }
+
   hamBtn.addEventListener('click', () => {
     hamBtn.classList.toggle('open');
     pageLogic.toggleMenuPg();
   });
+
+  return {
+    toggleHamBtn,
+  };
 })();
 
 export { hamBtnLogic };
