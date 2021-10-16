75ed341a21f5dd52ee012181086d0c7ecc8db61d
Edgar <ecarrillo046@gmail.com>
2021-10-16 12:41:42 -0700

    initialize page arr in local storage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './styles/reset.css';
 import './styles/main.css';
 import './modules/ham-btn.js';
+import './modules/page-switch-logic.js';
+
+let homePage = '.home-pg';
+let menuPage = '.menu-pg';
+let aboutPage = '.about-pg';
+
+if (!localStorage.getItem('pageArr')) {
+  let pageArr = [];
+  pageArr.push(homePage);
+  pageArr.push(menuPage);
+  pageArr.push(aboutPage);
+  localStorage.setItem('pageArr', JSON.stringify(pageArr));
+}
