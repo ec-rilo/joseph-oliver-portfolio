9725786d98c0d6dfbd5db06dc7d4d329b07a5598
Edgar <ecarrillo046@gmail.com>
2021-10-24 23:03:36 -0700

    Add contact-pg to local storage

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ if (!localStorage.getItem('pageArr')) {
   let homePage = 'home-pg';
   let aboutPage = 'about-me-pg';
   let photogPage = 'photography-pg';
+  let contactPage = 'contact-pg';
 
   let pageArr = [];
   pageArr.push(homePage);
   pageArr.push(aboutPage);
   pageArr.push(photogPage);
+  pageArr.push(contactPage);
   localStorage.setItem('pageArr', JSON.stringify(pageArr));
 }
