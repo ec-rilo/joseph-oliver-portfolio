70bbc3ab70e0f4719e0e012738bfd4c1f5cfa4d2
Edgar <ecarrillo046@gmail.com>
2021-10-26 12:29:59 -0700

    Add logic for showing main error message

--- src/modules/contact-pg-validation.js
+++ src/modules/contact-pg-validation.js
@@ -1,4 +1,10 @@
 let formLogic = (() => {
+  const name = document.getElementById('name');
+  const email = document.getElementById('email');
+  const phone = document.getElementById('phone');
+  const message = document.getElementById('message');
+  const itemsArr = [name, email, phone, message];
+
   const form = document.querySelector('form');
   form.addEventListener('submit', (event) => {
     checkAllItemsForErrors();
@@ -7,35 +13,6 @@ let formLogic = (() => {
 
   const userMessage = document.getElementById('message');
   userMessage.addEventListener('input', () => countChars());
-
-  function checkAllItemsForErrors() {
-    const name = document.getElementById('name');
-    const email = document.getElementById('email');
-    const phone = document.getElementById('phone');
-    const message = document.getElementById('message');
-    const itemsArr = [name, email, phone, message];
-    itemsArr.forEach((item) => {
-      errorCheck(item);
-    });
-  }
-
-  function errorCheck(item) {
-    const errorContainer = item.parentElement.nextElementSibling;
-
-    if (item.validity.typeMismatch || !item.validity.valid) {
-      errorContainer.style.display = 'block';
-    } else {
-      errorContainer.style.display = 'none';
-    }
-
-    item.addEventListener('input', () => {
-      if (item.validity.typeMismatch || !item.validity.valid) {
-        errorContainer.style.display = 'block';
-      } else {
-        errorContainer.style.display = 'none';
-      }
-    });
-  }
 
   function countChars() {
     let text = document.getElementById('message').value;
@@ -47,6 +24,52 @@ let formLogic = (() => {
 
     document.getElementById('show-words-count').innerHTML = numChars;
   }
+
+  function checkAllItemsForErrors() {
+    itemsArr.forEach((item) => {
+      errorCheck(item);
+    });
+  }
+
+  function errorCheck(item) {
+    const itemError = item.parentElement.nextElementSibling;
+    const errorMsg = document.querySelector('.error-message');
+
+    if (item.validity.typeMismatch || !item.validity.valid) {
+      itemError.style.display = 'block';
+      errorMsg.classList.add('show');
+    } else {
+      itemError.style.display = 'none';
+    }
+
+    item.addEventListener('input', () => {
+      let errorCount = 0;
+
+      if (item.validity.typeMismatch || !item.validity.valid) {
+        itemError.style.display = 'block';
+      } else {
+        itemError.style.display = 'none';
+      }
+
+      itemsArr.forEach((input) => {
+        if (!input.validity.valid) {
+          errorCount += 1;
+        }
+      });
+
+      if (errorCount === 0) {
+        errorMsg.classList.add('success-message');
+        errorMsg.innerHTML = 'You are ready to submit!';
+      } else {
+        errorMsg.innerHTML = 'Please complete required fields.';
+        errorMsg.classList.remove('success-message');
+      }
+    });
+  }
+
+  // Check each input individually. If it has an error, increment error check.
+  // if it doesn't, decrement it.
+  // always initialize to zero when it's called again.
 })();
 
 export default formLogic;
