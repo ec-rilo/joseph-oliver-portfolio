901f3b6e7ecc079c547bd512e5d2c517509c4dba
Edgar <ecarrillo046@gmail.com>
2021-10-28 17:09:42 -0700

    Add logic that gives the nav bar a background color on pages that are not the home page

---
+++ src/modules/nav-bar.js
@@ -0,0 +1,22 @@
+let navBarLogic = (() => {
+  const navBar = document.querySelector('nav');
+
+  document.addEventListener('scroll', () => changeNavBG());
+
+  function changeNavBG() {
+    const homePg = JSON.parse(localStorage.getItem('pageArr'))[0];
+    const homePgVal = window.getComputedStyle(
+      document.querySelector(`.${homePg}`)
+    ).display;
+
+    if (homePgVal !== 'block') {
+      navBar.style.backgroundColor = '#4a4e69';
+      console.log('none');
+    } else {
+      navBar.removeAttribute('style');
+      console.log('block');
+    }
+  }
+})();
+
+export default navBarLogic;
